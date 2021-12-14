3bac63f4481f5bd7a16affee257f718b60aea8de
Saad Ejaz <ejazsaad0332@gmail.com>
2021-12-14 14:01:59 +0100

    adding num_child

--- tsig.c
+++ tsig.c
@@ -3,12 +3,21 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
+#define NUM_CHILD 7
+#define SLEEP_TIME 2.5
+
+int create_child();
 int child_labour();
 
-int main(){
-    // creating a single child process
-    pid_t process_id = fork();
+int num_generated = 2;
 
+int main(){
+    create_child();
+    return 0;
+}
+
+int create_child(){
+    pid_t process_id = fork();
     // success in process creation
     if (process_id == 0){
         printf("parent[%d]: child process successfully created with PID: %d.\n", getppid(), getpid());
@@ -20,17 +29,21 @@ int main(){
     }
     // prints after processes all exit
     else{
-        wait(NULL);
-        printf("parent[%d]: all children processes completed.\n", getpid());
+        if (num_generated <= NUM_CHILD){
+            num_generated++;
+            create_child();
+        }
+        else{
+            wait(NULL);
+            printf("parent[%d]: all children processes completed.\n", getpid());
+        }
     }
-
-    return 0;
 }
 
 int child_labour(){
     int c_id = getpid();
     printf("child[%d]: process with parent PID %d started.\n", c_id, getppid());
-    sleep(5);
+    sleep(SLEEP_TIME);
     printf("child[%d]: process completed.\n", c_id);
     return 0;
 }
