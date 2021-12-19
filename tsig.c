775fdd020a965afd1b39c3284ae50d46f0210642
Saad Ejaz <ejazsaad0332@gmail.com>
2021-12-19 09:42:36 +0100

    added child process kill after fail

--- tsig.c
+++ tsig.c
@@ -2,17 +2,16 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <sys/wait.h>
-#include <string.h>
-#include <stdlib.h>
 
-#define NUM_CHILD 4
-#define SLEEP_TIME 1
+#define NUM_CHILD 5
+#define SLEEP_TIME 2
 
 int create_child();
 int child_labour();
 
 int num_generated = 1;
 int children_pids[NUM_CHILD];
+int wstatus;
 
 int main(){
     create_child();
@@ -20,9 +19,10 @@ int main(){
 }
 
 int create_child(){
-    // a recursive function in which only the parent procreates :/
+    /*a recursive function in which only the parent procreates :| */
 
     pid_t process_id = fork();
+    
     // success in process creation
     if (process_id == 0){
         printf("parent[%d]: child process successfully created with PID: %d.\n", getppid(), getpid());
@@ -30,19 +30,33 @@ int create_child(){
     }
     // error in process creation
     else if (process_id < 0){
-        printf("parent[%d]: failed to create a child process.\n", getppid());
+        printf("parent[%d]: failed to create a child process.\n", getpid());
+
+        // latest procreation failed, kill all children with SIGTERM (or a G3) ;(
+        for (int i = 0; i < NUM_CHILD; i++){
+            if (children_pids[i]){
+                int c_id = getpid();
+                int child_id = children_pids[i];
+                int check = kill(children_pids[i], SIGTERM);
+                if (check == 0){
+                    printf("parent[%d]: child process with id %d sent a termination signal.\n", c_id, child_id);
+                    int sig = waitpid(child_id, &wstatus, WUNTRACED);
+                    if (sig){
+                        if (WTERMSIG(wstatus) == SIGTERM) printf("parent[%d]: child process with id %d ended with a SIGTERM signal.\n", c_id, child_id);
+                        else printf("parent[%d]: child process with id %d ended with a %d signal.\n", c_id, child_id, WTERMSIG(wstatus));
+                    }
+                    else printf("parent[%d]: child process with id %d was not ended with a signal.\n", c_id, child_id);
+                } 
+                else printf("parent[%d]: could not send termination signal to child process with id %d.\n", getpid(), child_id);
+            } 
+        }
+        return 0;
     }
     // prints after processes all exit
     else{
         if (++num_generated <= NUM_CHILD){
             // keep track of children so they don't get lost - they are still young.
             children_pids[num_generated - 2] = process_id;
-
-            int status;
-            int k = waitpid(process_id, &status, WNOHANG);
-
-            printf("%d\n", k);
-
             create_child();
         }
         else{
@@ -50,9 +64,8 @@ int create_child(){
             children_pids[num_generated - 2] = process_id;
             wait(NULL);
             printf("parent[%d]: all children processes completed.\n", getpid());
-            printf("%d, %d, %d, %d\n", children_pids[0], children_pids[1], children_pids[2], children_pids[3]);
-            return 0;
         }
+        return 0;
     }
 }
 
